a3a3c7a9e89763b8d75b5f8a9a0005968ac32225
Amanda Thorne <manda.thorne@gmail.com>
2016-10-10 11:54:59 -0500

    styles consistent with example

--- app/scripts/Components/CategoryNav.js
+++ app/scripts/Components/CategoryNav.js
@@ -8,7 +8,7 @@ const CategoryNav = React.createClass({
         <h1 className="category-header">NYC Transportation Categories</h1>
         <nav className="category-nav">
           <ul className="category-list">
-            <li><a href="/#airports" smooth={true} duration={500}><img id="plane-icon" src="../../assets/icon-airplane.png"/><div><p>Airports</p></div></a></li>
+            <li><a href="/#airports"><img id="plane-icon" src="../../assets/icon-airplane.png"/><div><p>Airports</p></div></a></li>
             <li><a href="/#cabs"><img id="taxi-icon" src="../../assets/icon-taxi.png"/><div><p>Cabs</p></div></a></li>
             <li><a href="/#subway"><img id="subway-icon" src="../../assets/icon-subway.png"/><div><p>Subways</p></div></a></li>
             <li><a href="/#bus"><img id="bus-icon" src="../../assets/icon-bus.png"/><div><p>Bus</p></div></a></li>
