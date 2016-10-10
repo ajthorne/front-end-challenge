2153bf6b5d1030d674298cbba8cd73fb4499726f
Amanda Thorne <manda.thorne@gmail.com>
2016-10-09 22:38:22 -0500

    footer styled

--- app/scripts/Components/Footer.js
+++ app/scripts/Components/Footer.js
@@ -4,9 +4,9 @@ const Footer = React.createClass({
   render: function () {
     return (
       <footer>
-        <p>The rates or savings advertised are not necessarily available from all providers or advertisers listed on our site. Your actual rates or savings will vary based on factors like your driving history, coverage limits, deductibles, type of vehicle, location, & more. The Zebra is a licensed insurance broker, but we do not underwrite insurance policies.</p>
+        <p className="footer-info">The rates or savings advertised are not necessarily available from all providers or advertisers listed on our site. Your actual rates or savings will vary based on factors like your driving history, coverage limits, deductibles, type of vehicle, location, & more. The Zebra is a licensed insurance broker, but we do not underwrite insurance policies.</p>
 
-        <p>© 2016 Insurance Zebra. All Rights Reserved.</p>
+        <p className="footer-copyright">© 2016 Insurance Zebra. All Rights Reserved.</p>
 
         <p>Use of Insurance Zebra Insurance Services (DBA TheZebra.com) is subject to our Terms of Service, Privacy Policy and Licenses.</p>
       </footer>
